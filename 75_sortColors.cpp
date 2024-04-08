566c400c91a865cccffcef3f5a3b00e4ebaa5beb
Tuhassinul Arnob <t.arnob69@gmail.com>
2024-04-08 13:53:00 +0600

    Add files via upload

--- 75_sortColors.cpp
+++ 75_sortColors.cpp
@@ -3,7 +3,13 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        for(int i = 0;i < nums.size();i++){
+            for(int j = i+1;j < nums.size();j++){
+                if(nums[j] < nums[i]){
+                    swap(nums[j],nums[i]);
+                }
+            }
+        }
     }
 };
 
